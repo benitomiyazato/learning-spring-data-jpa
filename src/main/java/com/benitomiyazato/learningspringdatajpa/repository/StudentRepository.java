2aa0904bfa5c381f62819710b2717e231ad317d5
bemiyaz <benito.miya172@gmail.com>
2022-09-08 14:40:33 -0300

    fixed @Param annotation

--- src/main/java/com/benitomiyazato/learningspringdatajpa/repository/StudentRepository.java
+++ src/main/java/com/benitomiyazato/learningspringdatajpa/repository/StudentRepository.java
@@ -3,6 +3,7 @@ package com.benitomiyazato.learningspringdatajpa.repository;
 import com.benitomiyazato.learningspringdatajpa.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,11 +15,11 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     List<Student> findStudentByFirstNameIgnoreCase(String joao);
 
     @Query("SELECT s FROM Student s WHERE s.email = :studentEmail")
-    Student getStudentByEmail(String studentEmail);
+    Student getStudentByEmail(@Param("studentEmail") String studentEmail);
 
     @Query("SELECT s FROM Student s WHERE s.guardian.email = :guardianEmail")
-    List<Student> getStudentByGuardianEmail(String guardianEmail);
+    List<Student> getStudentByGuardianEmail(@Param("guardianEmail") String guardianEmail);
 
     @Query("SELECT s.firstName FROM Student s WHERE s.email = :studentEmail")
-    String getStudentFirstNameByEmail(String studentEmail);
+    String getStudentFirstNameByEmail(@Param("studentEmail") String studentEmail);
 }

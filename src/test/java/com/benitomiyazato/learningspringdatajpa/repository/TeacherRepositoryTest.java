d272b48d1fd441140499486d9c2f8b4130e2eed7
bemiyaz <benito.miya172@gmail.com>
2022-09-09 14:02:02 -0300

    many to one relationship

---
+++ src/test/java/com/benitomiyazato/learningspringdatajpa/repository/TeacherRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.benitomiyazato.learningspringdatajpa.repository;
+
+import com.benitomiyazato.learningspringdatajpa.entity.Course;
+import com.benitomiyazato.learningspringdatajpa.entity.Teacher;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TeacherRepositoryTest {
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    @Test
+    void saveTeacher(){
+        Course courseSQL = Course.builder()
+                .title("MySQL")
+                .credit(8)
+                .build();
+
+        Course coursePython = Course.builder()
+                .title("Python")
+                .credit(11)
+                .build();
+
+        Teacher teacherToSave = Teacher.builder()
+                .firstName("Eduardo")
+                .lastName("Silva")
+                .courses(List.of(courseSQL, coursePython))
+                .build();
+
+        teacherRepository.save(teacherToSave);
+    }
+}
